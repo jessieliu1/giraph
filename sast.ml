21db32b422607f90740c09fee1d89f53737927cc
Jennifer Bi <jb3495@columbia.edu>
2017-12-10 02:10:12 -0500

    more sast print fixes

--- sast.ml
+++ sast.ml
@@ -91,48 +91,51 @@ let sstring_of_typ = function
   | Void -> "void"
 
 
-let rec sstring_of_expr = function
+let rec string_of_expr = function
     SBool_Lit(true) -> "true"
   | SBool_Lit(false) -> "false"
-  | SInt_Lit(l) -> sstring_of_int l
+  | SInt_Lit(l) -> string_of_int l
   | SString_Lit(l) -> l
-  | SFloat_Lit(l) -> sstring_of_float l
-  | SId(s) -> s
-  | SBinop(e1, o, e2) ->
-      sstring_of_expr e1 ^ " " ^ sstring_of_op o ^ " " ^ sstring_of_expr e2
-  | SUnop(o, e) -> sstring_of_uop o ^ sstring_of_expr e
-  | SAssign(v, e) -> v ^ " = " ^ sstring_of_expr e
-  | SCall(f, el) ->
-      f ^ "(" ^ String.concat ", " (List.map sstring_of_expr el) ^ ")"
-  | SGraph(node_l, edge_l) ->
+  | SFloat_Lit(l) -> string_of_float l
+  | SId(s, t) -> s ^ ":" ^ string_of_typ t
+  | SBinop(e1, o, e2, t) ->
+      string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^
+      string_of_expr e2 ^ ":" ^ string_of_typ t
+  | SUnop(o, e, t) -> string_of_uop o ^ string_of_expr e ^ ":" ^ string_of_typ t
+  | SAssign(v, e, t) -> v ^ " = " ^ string_of_expr e^ ":" ^ string_of_typ t
+  | SCall(f, el, t) ->
+      f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"^ ":" ^ string_of_typ t
+  | SGraph(node_l, edge_l, t) ->
     "[" ^ String.concat ", " node_l ^ "] " ^
     "[" ^ String.concat ", " (List.map (fun(a,b) -> "(" ^ a ^ "," ^ b ^ ")") edge_l) ^ "]"
   | SNoexpr -> ""
 
-let rec sstring_of_stmt = function
-    Block(stmts) ->
-      "{\n" ^ String.concat "" (List.map sstring_of_stmt stmts) ^ "}\n"
-  | Expr(expr) -> sstring_of_expr expr ^ ";\n";
-  | Vdecl(t, id, x) -> sstring_of_typ t ^ " " ^ id ^ ";\n"
-  | Return(expr) -> "return " ^ sstring_of_expr expr ^ ";\n";
-  | If(e, s, Block([])) -> "if (" ^ sstring_of_expr e ^ ")\n" ^ sstring_of_stmt s
-  | If(e, s1, s2) ->  "if (" ^ sstring_of_expr e ^ ")\n" ^
-      sstring_of_stmt s1 ^ "else\n" ^ sstring_of_stmt s2
-  | For(e1, e2, e3, s) ->
-      "for (" ^ sstring_of_expr e1  ^ " ; " ^ sstring_of_expr e2 ^ " ; " ^
-      sstring_of_expr e3  ^ ") " ^ sstring_of_stmt s
-  | While(e, s) -> "while (" ^ sstring_of_expr e ^ ") " ^ sstring_of_stmt s
+let rec string_of_stmt = function
+    SBlock(stmts) ->
+      "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
+  | SExpr(expr, t) -> string_of_expr expr ^ ":" ^ string_of_typ t^ ";\n";
+  | SVdecl(t, id, x) -> string_of_typ t ^ " " ^ id ^ ";\n"
+  | SReturn(expr) -> "return " ^ string_of_expr expr ^ ";\n";
+  | SIf(e, s, SBlock([])) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s
+  | SFor(e1, e2, e3, s) ->
+      "for (" ^ string_of_expr e1  ^ " ; " ^ string_of_expr e2 ^ " ; " ^
+      string_of_expr e3  ^ ") " ^ string_of_stmt s
+  | SFor_Node(s1,  e, s2) ->
+      "for ("^ s1 ^" ; " ^ string_of_expr e ^ " ; ) " ^ string_of_stmt s2
+  | SFor_Edge(s1,  e, s2) ->
+      "for ("^ s1 ^" ; " ^ string_of_expr e ^ " ; ) " ^ string_of_stmt s2
+  | SWhile(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
 
 
-let sstring_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n"
+let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n"
 
-let sstring_of_fdecl fdecl =
-  sstring_of_typ fdecl.f_typ ^ " " ^
-  fdecl.f_name ^ "(" ^ String.concat ", " (List.map snd fdecl.f_formals) ^
+let string_of_fdecl sfdecl =
+  string_of_typ sfdecl.sf_typ ^ " " ^
+  sfdecl.sf_name ^ "(" ^ String.concat ", " (List.map snd sfdecl.sf_formals) ^
   ")\n{\n" ^
-  String.concat "" (List.map sstring_of_stmt fdecl.f_body) ^
+  String.concat "" (List.map string_of_stmt sfdecl.sf_body) ^
   "}\n"
 
-let sstring_of_program (vars, funcs) =
-  String.concat "" (List.map sstring_of_vdecl vars) ^ "\n" ^
-  String.concat "\n" (List.map sstring_of_fdecl funcs)
+let string_of_program (vars, funcs) =
+  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
+  String.concat "\n" (List.map string_of_fdecl funcs)
